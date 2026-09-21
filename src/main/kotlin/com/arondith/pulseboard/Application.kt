d46ec93e49b28504627667ba7560bd747c8ff9ef
Arondite <Aronditee@gmail.com>
2026-09-21 20:54:57 +0800

    Serve PulseBoard dashboard from root route

--- src/main/kotlin/com/arondith/pulseboard/Application.kt
+++ src/main/kotlin/com/arondith/pulseboard/Application.kt
@@ -12,6 +12,7 @@ import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
@@ -62,5 +63,6 @@ fun Application.module() {
 
     routing {
         incidentRoutes(service)
+        staticResources("/", "static", index = "index.html")
     }
 }
